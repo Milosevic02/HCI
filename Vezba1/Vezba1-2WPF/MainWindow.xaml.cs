ae0331e02adbd98cf683bac94380cd7b7fce4c44
Dragan <milosevicdragan002@gmail.com>
2024-03-15 22:48:34 +0100

    Base project

---
+++ Vezba1/Vezba1-2WPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using Vezba1_2;
+
+namespace Vezba1_2WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NameTextBox.Text = "Input student name";
+
+            DepartmentComboBox.ItemsSource = Constants.departments;
+        }
+
+        private void CreateStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            Student newStudent = CreateFromFormData();
+
+            StudentInfoWindow newWindow = new StudentInfoWindow(newStudent);
+            newWindow.ShowDialog();
+        }
+
+        private Student CreateFromFormData()
+        {
+            if (MaleRadioButton.IsChecked == true)
+            {
+                return new Student(NameTextBox.Text, SurnameTextBox.Text, 'M', DepartmentComboBox.SelectedValue.ToString());
+            }
+            else
+            {
+                return new Student(NameTextBox.Text, SurnameTextBox.Text, 'F', DepartmentComboBox.SelectedValue.ToString());
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
