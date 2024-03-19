58709d4c93d414756747f14dd0942449f069e5e0
Dragan <milosevicdragan002@gmail.com>
2024-03-19 16:15:18 +0100

    Remove and Add student

--- Vezbe2/Vezbe2/Pages/DataTablePage.xaml.cs
+++ Vezbe2/Vezbe2/Pages/DataTablePage.xaml.cs
@@ -24,19 +24,46 @@ namespace Vezbe2.Pages
     /// </summary>
     public partial class DataTablePage : Page
     {
+        public ObservableCollection<Student> Students { get; set; }
+
+        MainWindow mainWindow;
+
         public DataTablePage()
         {
             InitializeComponent();
+
+            mainWindow = (MainWindow)Application.Current.MainWindow;
+
+            Students = mainWindow.Students;
+
+            DataContext = this;
         }
 
         private void RemoveStudentButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            if(Students.Count > 0)
+            {
+                if(StudentsDataGrid.SelectedIndex != -1)
+                {
+                    Students.RemoveAt(StudentsDataGrid.SelectedIndex);
+                    mainWindow.ShowToastNotification(new ToastNotification("Success", "Student removed from the Data Table", NotificationType.Success));
+
+                }
+                else
+                {
+                    mainWindow.ShowToastNotification(new ToastNotification("Error", "A student must be selected first!", NotificationType.Error));
+                }
+            }
+            else
+            {
+                mainWindow.ShowToastNotification(new ToastNotification("Error","Cannot delete from empty table!",NotificationType.Error));
+            }
+
         }
 
         private void AddStudentButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            this.NavigationService.Navigate(new Uri("Pages/AddStudentPage.xaml", UriKind.RelativeOrAbsolute));
         }
     }
 }
